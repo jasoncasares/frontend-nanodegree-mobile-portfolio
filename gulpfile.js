ec7964b2ecaaabe012934085b1eeda0ff3cdb2b5
Jason Casares <jasonc@csdrafting.com>
2016-02-08 14:27:14 -0800

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-var imageminJpegoptim = require('imagemin-jpegoptim');
  
 gulp.task('minify-inline', function() {
   gulp.src('src/*.html')
@@ -10,13 +9,8 @@ gulp.task('minify-inline', function() {
 });
 
 gulp.task('image', function () {
-	return gulp.src('src/img/*.jpg', 'src/views/images/*.jpg')
+	return gulp.src('src/**/*.jpg')
 		.pipe(imageminJpegRecompress({loops: 3})())
 		.pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('imagejpeg', function () {
-	return gulp.src('src/img/*.jpg', 'src/views/images/*.jpg')
-		.pipe(imageminJpegoptim({progressive: true})())
-		.pipe(gulp.dest('dist/images'));
-});
