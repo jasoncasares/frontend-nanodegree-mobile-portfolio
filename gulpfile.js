7c20e5ca938bdd333f4eabf53f608296fa60c926
Jason Casares <jasonc@csdrafting.com>
2016-02-08 13:22:55 -0800

    add gulp plug in for image resize

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
+var imageResize = require('gulp-image-resize');
  
 gulp.task('minify-inline', function() {
   gulp.src('src/*.html')
     .pipe(minifyInline())
     .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('image', function () {
+  gulp.src('src/img/*')
+    .pipe(imageResize({ 
+      width : 100,
+      height : 100,
+      crop : true,
+      upscale : false
+    }))
+    .pipe(gulp.dest('dist/img'));
 });
