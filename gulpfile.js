84db0f250ee7011051d2d57f85eda81d7afaecc4
Jason Casares <jasonc@csdrafting.com>
2016-02-08 14:55:13 -0800

    add gulp plug in

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+var inlineCss = require('gulp-inline-css');
  
 gulp.task('minify-inline', function() {
   gulp.src('src/*.html')
@@ -14,3 +15,8 @@ gulp.task('image', function () {
 		.pipe(gulp.dest('dist/images'));
 });
 
+gulp.task('css', function() {
+    return gulp.src('src/*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('dist/css'));
+});
