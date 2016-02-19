1e6df6e74ed35404cd2b4e2d2109b52f8995a86b
Jason Casares <jasonc@csdrafting.com>
2016-02-19 11:54:52 -0800

    docs: fix user comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 var inlineCss = require('gulp-inline-css');
-var imageResize = require('gulp-image-resize');
  
 gulp.task('minify-inline', function() {
   gulp.src('src/*.html')
@@ -16,18 +15,6 @@ gulp.task('image-1', function () {
 		.pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('image-2', function () {
-	return gulp.src('src/views/images/*.jpg')
-		.pipe(imageminJpegRecompress({loops: 3})())
-		.pipe(imageResize({ 
-	      width : 100,
-	      height : 100,
-	      crop : true,
-	      upscale : false
-	    }))
-		.pipe(gulp.dest('dist/views/images'));
-});
-
 gulp.task('css', function() {
     return gulp.src('src/*.html')
         .pipe(inlineCss())
@@ -36,4 +23,4 @@ gulp.task('css', function() {
 
 //default task
 
-gulp.task('default', ['minify-inline', 'image-1', 'image-2', 'css']);
+gulp.task('default', ['minify-inline', 'image-1', 'css']);
